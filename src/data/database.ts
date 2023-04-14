83c8c3f05257ab4070d6dfcfd6e58ec4c53cc316
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-04-14 08:37:22 +0200

    database.ts - readDB()
    Added a readDB() function which allows retrieving all data from the database. It returns an array of objects if the connection is successful else return false.

--- src/data/database.ts
+++ src/data/database.ts
@@ -5,20 +5,30 @@ import { createClient } from "@supabase/supabase-js";
  * @returns A SupaBase Client connected to the Database
  */
 function connectSupaBase() {
-	const supabaseUrl: string = 'https://zluqgjkxodwjmkfnppyr.supabase.co'
-	const supabaseKey: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsdXFnamt4b2R3am1rZm5wcHlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyODk0NDQsImV4cCI6MTk5Njg2NTQ0NH0.-73kdnKGSpjvxMAJDetmkoOTxrbEC-GYmDKBm5K9jVU"
-	const supabase = createClient(supabaseUrl, supabaseKey)
+	const supabaseUrl: string = 'https://zluqgjkxodwjmkfnppyr.supabase.co';
+	const supabaseKey: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsdXFnamt4b2R3am1rZm5wcHlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyODk0NDQsImV4cCI6MTk5Njg2NTQ0NH0.-73kdnKGSpjvxMAJDetmkoOTxrbEC-GYmDKBm5K9jVU";
+	const supabase = createClient(supabaseUrl, supabaseKey);
 	return supabase;
 }
 
 /**
- * 
- * @param values A object with all propriety and their values to be inserted into the Database, 
- * propriety must match column name of database to be inserted with sucess. 
- * @return True if values are inserted with sucess, false if a error happened
+ * @param values An object containing all properties and their corresponding values to be inserted into the database.
+ * @return True if the values were inserted successfully, false if an error occurred.
 */
 export default async function insertDB(values: object) {
 	const supabase = connectSupaBase();
-	const response = await supabase.from('hrnet').insert(values)
-	if (response.status === 201) { return true } else { return false }
+	const response = await supabase.from('hrnet').insert(values);
+	if (response.status !== 201) { return false;} 
+	return true; 
+}
+
+/**
+ * @returns Return all data from the database as a array of object or return false if the operation fails.
+ */
+export async function readDB() {
+	const supabase = connectSupaBase();
+	const response = await supabase.from('hrnet').select('*');
+	const responseData = response.data || null;
+	if (!responseData || response.status !== 200) { return false; }; 
+	return responseData;
 }
