2e9651cd7365df319d812512ec6cb6c97223fd15
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-04-18 17:47:48 +0200

    Table Employee #1
    Load Data from the database.
    Display the data.
    
    Allow to search, allow to filter by column, pagination set to 100 by default.

--- src/components/TableEmployee/TableEmployee.tsx
+++ src/components/TableEmployee/TableEmployee.tsx
@@ -1,21 +1,31 @@
 
 import { Grid } from 'gridjs-react';
 import "gridjs/dist/theme/mermaid.css";
+import "./TableEmployee.scss";
 
 export interface TableEmployeeProps {
-	rowsData: Array<Array<string>>
-	columnsData: Array<string>
+	rowsValues: Array<string>
+	columnsValues: Array<string>
 }
-export default function TableEmployee({ rowsData, columnsData }: TableEmployeeProps): JSX.Element {
+
+export default function TableEmployee({ rowsValues, columnsValues }: TableEmployeeProps): JSX.Element {
+
+
 
 	return (
+
 		<Grid
-			data={rowsData}
-			columns={columnsData}
+			data={rowsValues}
+			columns={columnsValues}
 			search={true}
+			resizable={true}
 			pagination={{
-				limit: 20,
+				enabled: true,
+				summary: true,
+				limit: 100,
 			}}
+			sort={true}
 		/>
+
 	);
 }
--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -1,13 +1,53 @@
 
 import TableEmployee from "../components/TableEmployee/TableEmployee";
+import { useState, useEffect } from "react";
+import { readDB } from "../data/database";
+import "../globalStyle.scss";
+import { Link } from "react-router-dom";
+import { homePath } from "../utils/routesPath";
 
 export default function EmployeeList(): JSX.Element {
+
+	const [isLoading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<boolean>(false);
+	const [rowsData, setRowsData] = useState<string[]>([]);
+	const [columnsData, setColumnsData] = useState<string[]>([]);
+
+	const loadData = async () => {
+		const data: any = await readDB();
+
+		if (data) {
+			const columnsData: Array<string> = Object.getOwnPropertyNames(data[0]);
+			const rowsData: Array<string> = data.map((object: any) => Object.values(object));
+			setColumnsData(columnsData);
+			setRowsData(rowsData);
+		}
+		else {
+			setError(true);
+		}
+
+		setLoading(false);
+	}
+
+
+	useEffect(() => {
+		loadData();
+	}, [])
+
+
+
 	return (
-		<TableEmployee rowsData={[
-			['John', 'john@example.com'],
-			['Mike', 'mike@gmail.com']
-		]}
-			columnsData={['Name', 'Email']} />
+		<div id="employee-div" className="container">
+			<h1>Current Employees</h1>
+			{!isLoading && !error ?
+				(<TableEmployee
+					rowsValues={rowsData}
+					columnsValues={columnsData}
+				/>) :
+				(<h2>{error ? "Error cannot access data" : "Loading..."}</h2>)
+			}
+			<Link to={homePath}>Home</Link>
+		</div>
 	);
 }
 
