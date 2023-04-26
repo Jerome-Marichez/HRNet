90e4bb9f65802946b4206e5bd3b9c3a9a9486d70
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-04-26 07:46:31 +0200

    Delete old files CRA
    Delete old files who was created at began of the project

--- src/components/TableEmployee/TableEmployee.test.tsx
+++ src/components/TableEmployee/TableEmployee.test.tsx
@@ -4,6 +4,9 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import TableEmployee from './TableEmployee';
 import { randomInt } from 'crypto';
 
+// TODO Test to be done check if two function have be called need to mock them 
+// instead of check grid visiblity 
+
 describe('TableEmployee Tests', () => {
 
 	const columnsTest: Array<string> = ["firstName", "lastName"];
@@ -34,10 +37,31 @@ describe('TableEmployee Tests', () => {
 		/>, { wrapper: BrowserRouter });
 
 		const select: HTMLElement = screen.getByTestId("pagination");
+		expect(select).toBeVisible();
+
 		fireEvent.change(select, { target: { value: paginationRange[randomIndex] } })
 		// Can't count <td> number since <Grid/> is a private component
+		const grid: HTMLElement = screen.getByRole('grid');
+		expect(grid).toBeInTheDocument();
 	});
 
+	test('Test Zoom Feature', async () => {
+		const zoomRange = [5, 10, 25, 50, 100];
 
+		// Select a Random Index 
+		const randomIndex = randomInt(0, zoomRange.length);
 
+		render(<TableEmployee
+			columnsValues={columnsTest}
+			rowsValues={rowsTest}
+			zoomRange={zoomRange}
+		/>, { wrapper: BrowserRouter });
+
+		const select: HTMLElement = screen.getByTestId("zoom");
+		expect(select).toBeVisible();
+
+		fireEvent.change(select, { target: { value: zoomRange[randomIndex] } })
+		const grid: HTMLElement = screen.getByRole('grid');
+		expect(grid).toBeInTheDocument();
+	});
 }); 
