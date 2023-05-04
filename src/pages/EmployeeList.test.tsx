8f5f6eb589386d9c04c1f3b82a99f2af35273af5
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-05-04 13:15:22 +0200

    Fixed EmployeeList - Test Logic
    They was a issue about logic of this test, because i didn't check before that no error happen if all go fine with data fetched

--- src/pages/EmployeeList.test.tsx
+++ src/pages/EmployeeList.test.tsx
@@ -4,20 +4,21 @@ import EmployeeList from './EmployeeList';
 import { readDB } from '../data/database';
 
 
-
 describe('EmployeeList', () => {
 
-	test('displays loading message then "Current Employees" when data is being fetched', async () => {
+	test('Display a loading message while the data is being fetched, then display "Current Employees" when the data is ready.', async () => {
+		jest.spyOn(require('../data/database'), 'readDB').mockReturnValue([{ employee: "John" }, { employee: "Smith" }]);
+
 		render(<EmployeeList />);
-		expect(screen.getByText('Loading 🖳')).toBeVisible();
+		const h2Element = screen.getByText('Loading 🖳');
 		expect(await screen.findByText('Current Employees')).toBeVisible();
+		setTimeout(() => 500);
+		expect(h2Element).not.toContainHTML("Error");
 	});
 
-	test('displays loading message then a error message', async () => {
-		jest.mock('../data/database', () => ({
-			readDB: jest.fn(() => false)
-		}));
-		
+test('Display a loading message while attempting to read the database, then display an error message if there is an error', async () => {
+		jest.spyOn(require('../data/database'), 'readDB').mockReturnValue(false);
+
 		render(<EmployeeList />);
 		expect(screen.getByText('Loading 🖳')).toBeVisible();
 		expect(await screen.findByText('Error cannot access data 😭')).toBeVisible();
