d1e02a82726306070d12e53d0586bef07b1b2dce
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-05-04 11:15:09 +0200

    Test Coverage 100% Home.tsx
    Written test for Home.tsx with a coverage of 100%.
    
    Testing if main title and link it's displayed and have the correct path.

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -5,12 +5,13 @@ import { employeePath } from "../utils/routesPath";
 import { BrowserRouter } from "react-router-dom";
 
 describe("Home component", () => {
-	it("renders the HRnet title & View Current Employees link", async() => {
+	it("renders the HRnet title & View Current Employees link", () => {
 		render(<Home />, { wrapper: BrowserRouter });
-		const titleElement = screen.getByText("HRnet");
-		expect(titleElement).toBeVisible();
 
-		const linkElement = await screen.findByText("View Current Employees");
+		const titleElement = screen.getByText("HRnet");
+		expect(screen.getByText("HRnet")).toBeVisible();
+
+		const linkElement = screen.getByText("View Current Employees");
 		expect(linkElement).toBeVisible();
 		expect(linkElement.getAttribute("href")).toBe(`/${employeePath}`);
 	});
